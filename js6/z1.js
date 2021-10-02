9f946521d00d65b35e7b8979ec779c1887d8a253
SLmamba <74455750+SLmamba@users.noreply.github.com>
2021-10-02 17:49:36 +0300

    Add files via upload

---
+++ js6/z1.js
@@ -0,0 +1,28 @@
+/*let name = `ПЕТРО`;
+let yrok = `МАТЕМАТИКА`;
+let massive = [];
+
+for(i=0; i < 5; i++){
+    massive[i] = prompt("Введіть оцінку")
+}
+console.log(`${massive}`);
+
+
+
+let name = `ПЕТРО`;
+let yrok = `МАТЕМАТИКА`;
+let ochinka = [10, 12, 11, 2];
+
+console.log(`Вітаю ${name}! Кількість оцінок з придмету ${yrok} рувна ${ochinka.length} Останя оцінка - ${ochinka[ochinka.length -1]} `);
+
+
+
+let massive = [12, 13, 78, 98, 109, 65];
+for(i=1; i < 5; i++){
+    console.log(`Привіт, я цифра ${massive[i]}`)    
+}
+
+
+let vikusih = [13, 14, 16, 11];
+vikusih[13]=20;
+console.log(vikusih[2]);*/
